574e181f11097bc48aec07353c3a90fc53009302
rfegamas <57278672+rfegamas@users.noreply.github.com>
2021-08-01 20:49:52 -0700

    flask update

--- src/api.py
+++ src/api.py
@@ -1,23 +1,31 @@
+import flask
 import pandas as pd
 
-file = "../data/small.csv"
+from flask import request
 
-# csv = pd.read_csv("../data/small.csv")
+FILE = "../data/small.csv"
 
-# protein_more_than = csv[csv["Protein (g)"] > 3.2]
-# print(protein_more_than)
+food_src = pd.read_csv(FILE)
+useless_data = ["Food code", "WWEIA Category number",
+                "WWEIA Category description"]
 
-food_src = pd.read_csv(file)
-
-useless_data = ['Food code', 'Main food description',
-                'WWEIA Category number', 'WWEIA Category description']
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
 
 
-def get_food_by_name(name):
+@app.route("/", methods=["GET"])
+def home():
+    return "<h1>FoodCountant</h1><p>An accountant, but for your food</p>"
+
+
+@app.route("/get-food-by-name", methods=["GET"])
+def get_food_by_name():
+    name = request.args.get("name", None)
+
     foods = food_src[food_src["Main food description"].str.contains(
         name, na=False, case=False)]
 
-    pruned_foods = foods.drop(useless_data, axis=1)
+    pruned_foods = foods.drop([useless_data, "Main food description"], axis=1)
     mean = pruned_foods.mean()
 
     food = {"food_name": name}
@@ -27,10 +35,16 @@ def get_food_by_name(name):
     return food
 
 
-def get_foods_by_nutrients(nutrient_name, nutrient_value):
-    return food_src[food_src[nutrient_name] >= nutrient_value]
+@app.route("/get-foods-by-nutrients", methods=["GET"])
+def get_foods_by_nutrients():
+    nutrient_name = request.args.get("nutrient_name", None)
+    nutrient_value = float(request.args.get("nutrient_value", None))
+
+    result = food_src[food_src[nutrient_name] >=
+                      nutrient_value].drop(useless_data, axis=1)
+
+    return result.to_dict()
 
 
-# get_food_by_name('milk')
-# tmp = get_foods_by_nutrients('Protein (g)', 3.3)
-# print(str(tmp))
+if __name__ == "__main__":
+    app.run()
